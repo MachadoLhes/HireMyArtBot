8282fdb6c3a61bb5bbe32ac6fa2b3592eba3cc86
Rodrigo Machado <souza.machado.rodrigo@gmail.com>
2020-08-24 18:43:18 -0300

    initial commit

--- script.py
+++ script.py
@@ -1,5 +1,40 @@
-import time
+import praw, requests
+from googletrans import Translator
 
-while True:
-	print("I am working!")
-	time.sleep(2)
+reddit = praw.Reddit(client_id = 'efGYc-LFh4aYow',
+					 client_secret = 'qsQhjkiEKTGlEQQiXBYMEOViFCc',
+					 username = 'hiremyartbot',
+					 password = 'm35L^7NmNTtBtnYS',
+					 user_agent = 'art_listener')
+
+translator = Translator()
+
+subreddit = reddit.subreddit('hungryartists')
+
+def create_bot_message(submission):
+	tittle_pt = translator.translate(submission.title, dest='pt', src='en')
+
+	message = ""	
+	message += tittle_pt.text + '\n'
+	message += submission.url + '\n'
+
+	return message
+
+def telegram_bot_sendtext(bot_message):
+    
+    bot_token = '1226525498:AAGDP1-n91fRbSskv1lzL_9cUXLeeKadBrg'
+    bot_chatID = '@hiremyartchannel'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+
+def main():
+	for submission in subreddit.stream.submissions():
+		if submission.link_flair_text == 'Hiring':
+			message = create_bot_message(submission)
+			telegram_bot_sendtext(message)		
+
+if __name__ == "__main__":
+    main()
